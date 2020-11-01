5ef4b998e950d3537f384b000d331e3b801c1253
Noreen-Jamil <noreenzubair8@gmail.com>
2020-11-01 21:00:13 +0500

    Updates

---
+++ precache-manifest.93e3d9716394d7abdf8ea158684038b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8146878f971016e476218702edeb1b63",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "63886adf62848363a137",
+    "url": "/react-todo-app/static/css/main.cf80a018.chunk.css"
+  },
+  {
+    "revision": "51086f1cb974d68505f5",
+    "url": "/react-todo-app/static/js/2.72297129.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-todo-app/static/js/2.72297129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63886adf62848363a137",
+    "url": "/react-todo-app/static/js/main.e6a532f9.chunk.js"
+  },
+  {
+    "revision": "77ef626dbdd4b1e53296",
+    "url": "/react-todo-app/static/js/runtime-main.3db00947.js"
+  },
+  {
+    "revision": "9c94cd3ab493131eddcb008764a45bfb",
+    "url": "/react-todo-app/static/media/bg3.9c94cd3a.png"
+  },
+  {
+    "revision": "35bd7103dfec0977f11223fe3714d667",
+    "url": "/react-todo-app/static/media/logo1.35bd7103.png"
+  }
+]);
